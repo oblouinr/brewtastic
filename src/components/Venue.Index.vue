64f84e022d933317d1275614a55b78c9fe95a125
David Paradis <dparadis@petalmd.com>
2016-06-03 13:16:03 -0400

    Use categories instead of name search

--- src/components/Venue.Index.vue
+++ src/components/Venue.Index.vue
@@ -21,11 +21,14 @@
     },
     methods: {
       fetchVenues: function () {
+
+        let categoryIds = ['56aa371ce4b08b9a8d57356c', '4bf58dd8d48988d117941735', '4bf58dd8d48988d11b941735', '50327c8591d4c4b30a586d5d']
         let clientId = Vue.config.keys.foursquare.clientId;
         let clientSecret = Vue.config.keys.foursquare.clientSecret;
-        let url = "https://api.foursquare.com/v2/venues/search?query=microbrasserie&ll=46.8131251,-71.2257053&client_id=" + clientId + "&client_secret="
-          + clientSecret + "&v=20160603";
-
+        let url = "https://api.foursquare.com/v2/venues/search?ll=46.8131251,-71.2257053&client_id="
+          + clientId + "&client_secret="
+          + clientSecret + "&v=20160603"
+          + "&categoryId=" + categoryIds.join(',') ;
         this.$http({url: url, method: 'GET'}).then(function (response) {
           let venues = response.data.response.venues.map(v => {
             v.distance = v.location.distance;
