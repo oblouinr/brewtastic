247543a34799f71f556d4b2683f4e8b4a848b119
David Paradis <dparadis@petalmd.com>
2016-06-03 12:32:38 -0400

    Add venues + distance and use foursquare api

--- src/components/Venue.Index.vue
+++ src/components/Venue.Index.vue
@@ -1,16 +1,18 @@
+
 <template>
   <div>
     <h1>Brasseries</h1>
     <ul>
       <li v-for="venue in venues">
-        <a v-link="{ name: 'venue', params: { venueId: venue.id }}">{{ venue.name }}</a>
+        {{ venue.name }} ({{ venue.distance }} meters)
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-  import Vue from 'vue'
+
+  import Vue from 'vue';
   export default {
     data () {
       return {
@@ -19,17 +21,17 @@
     },
     methods: {
       fetchVenues: function () {
-        console.log(Vue.config);
-        this.$set('venues', [
-          {
-            id: 1,
-            name: 'Korrigane'
-          },
-          {
-            id: 2,
-            name: 'Noctem'
-          }
-        ]);
+
+        var clientId = Vue.config.keys.foursquare.clientId;
+        var clientSecret = Vue.config.keys.foursquare.clientSecret;
+        var url = "https://api.foursquare.com/v2/venues/search?query=microbrasserie&ll=46.8131251,-71.2257053&client_id=" + clientId + "&client_secret="
+          + clientSecret + "&v=20160603";
+        this.$http({url: url, method: 'GET'}).then(function (response) {
+          var venues = response.data.response.venues.map(v => {
+            v.distance = v.location.distance;
+          });
+          this.$set('venues', response.data.response.venues);
+        });
       }
     },
     ready () {
